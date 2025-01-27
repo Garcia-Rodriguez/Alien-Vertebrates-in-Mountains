2ae664e90b2f8b494cf37f5e273833c317ca4b08
Adrián García-Rodríguez <60767341+Garcia-Rodriguez@users.noreply.github.com>
2025-01-27 21:56:02 +0100

    GLM.R

--- GLM.R
+++ GLM.R
@@ -1,8 +1,8 @@
-###GLMM Vertebrate invasions in Mountains of the World
+###GLMM Alien vertebrates in Mountains of the World
 PKGs <- c("raster","rlang", "rgbif", "dplyr","scrubr", "rgdal","maps", "maptools", "exactextractr","sp","spocc","plyr", "tidyverse", "pbapply", "sf", "ggplot2", "rgeos", "R.utils","data.table", "here", "rgdal",  "RColorBrewer",  "letsR", "rasterVis", "beepr", "circlize", "stringr", "moments", "diptest", "corrplot", "AER", "sjPlot", "sjmisc", "sjlabelled", "ggpubr", "blmeco","partR2", "magrittr", "glmm.hp", "GLMMadaptive", "gridExtra", "lme4", "furrr")
 sapply(PKGs, require, character.only = TRUE)
 
-setwd("your_path")
+setwd("your_path") ##Set the path in your machine
 
 ##Load data (Mountain level info, Alien Richness as response variable + predictors tested)
 data<-read.csv("~/DataPredictors.csv", sep=";")
@@ -39,29 +39,29 @@ dat.scaled2$Level_02<-as.factor(dat.scaled2$Level_02)
 dat.scaled2$Level_03<-as.factor(dat.scaled2$Level_03)
 dat.scaled2$geometry<-as.factor(dat.scaled2$Geometry)
 
-##Cross-taxonomic model
+##Cross-taxonomic model (All data combined)
 mod_neg.bi<-glmer.nb(AlienRich ~ offset(log(Area)) + BII  +  HumanPopDens + RoadDens + TimeCities + TimePorts  + Samp.completeness +  VccMg_tmn + VccMg_tmx  +  ElevRange + Rough + Geometry +  (1|Level_01/Level_02/Level_03) , data= dat.scaled2, control = glmerControl(optimizer = "bobyqa"))
-summary(mod_neg.bi)
+summary(mod_neg.bi) # n= 2696 mountains
 
 ###Fishes
 mod_neg.bi.fish<-glmer.nb(AlienFish ~ offset(log(Area)) + BII  +  HumanPopDens + RoadDens + TimeCities + TimePorts  + Samp.completeness +  VccMg_tmn + VccMg_tmx  +  ElevRange + Rough + Geometry +  (1|Level_01/Level_02/Level_03) , data= dat.scaled2[dat.scaled2$AlienFish>0,], control = glmerControl(optimizer = "bobyqa"))
-summary(mod_neg.bi.fish)
+summary(mod_neg.bi.fish) # n= 722 mountains
 
 ###Amphibians
 mod_neg.bi.amphs<-glmer.nb(AlienAmphibians ~ offset(log(Area)) + BII  +  HumanPopDens + RoadDens + TimeCities + TimePorts  + Samp.completeness +  VccMg_tmn + VccMg_tmx  +  ElevRange + Rough + Geometry +  (1|Level_01/Level_02/Level_03) , data= dat.scaled2[dat.scaled2$AlienAmphibians>0,], control = glmerControl(optimizer = "bobyqa"))
-summary(mod_neg.bi.amphs)
+summary(mod_neg.bi.amphs) # n= 429 mountains
 
 ###Reptiles
 mod_neg.bi.repts<-glmer.nb(AlienReptiles ~ offset(log(Area)) + BII  +  HumanPopDens + RoadDens + TimeCities + TimePorts  + Samp.completeness +  VccMg_tmn + VccMg_tmx  +  ElevRange + Rough + Geometry +  (1|Level_01/Level_02/Level_03) , data= dat.scaled2[dat.scaled2$AlienReptiles>0,], control = glmerControl(optimizer = "bobyqa"))
-summary(mod_neg.bi.repts)
+summary(mod_neg.bi.repts) # n= 498 mountains
 
 ###Birds
 mod_neg.bi.birds<-glmer.nb(AlienBirds ~ offset(log(Area)) + BII  +  HumanPopDens + RoadDens + TimeCities + TimePorts  + Samp.completeness +  VccMg_tmn + VccMg_tmx  +  ElevRange + Rough + Geometry +  (1|Level_01/Level_02/Level_03) , data= dat.scaled2[dat.scaled2$AlienBirds>0,], control = glmerControl(optimizer = "bobyqa"))
-summary(mod_neg.bi.birds)
+summary(mod_neg.bi.birds) # n= 2424 mountains
 
 ###Mammals
 mod_neg.bi.mamms<-glmer.nb(AlienMammals ~ offset(log(Area)) + BII  +  HumanPopDens + RoadDens + TimeCities + TimePorts  + Samp.completeness +  VccMg_tmn + VccMg_tmx  +  ElevRange + Rough + Geometry +  (1|Level_01/Level_02/Level_03) , data= dat.scaled2[dat.scaled2$AlienMammals>0,], control = glmerControl(optimizer = "bobyqa"))
-summary(mod_neg.bi.mamms)
+summary(mod_neg.bi.mamms) # n= 1549 mountains
 
 ###Plot effects
 models.nb<- list(mod_neg.bi,mod_neg.bi.fish,mod_neg.bi.amphs,mod_neg.bi.repts,mod_neg.bi.birds, mod_neg.bi.mamms)
